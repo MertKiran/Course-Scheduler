dbebdb76d543f1db7322331ccb335f24c6279b8c
Mert <35403585+MertKiran@users.noreply.github.com>
2018-01-13 20:57:39 +0300

    Update sql script.sql
    MySql scirpt

--- sql script.sql
+++ sql script.sql
@@ -1,42 +1,47 @@
+
 create database Schedularv3;
-CREATE TABLE bolumler (
-  birim varchar(20) NOT NULL,
-  birim_adi varchar(20)DEFAULT NULL,
-  PRIMARY KEY (birim)
-);
-
-
-CREATE TABLE dersler (
-  birim varchar(20) NOT NULL,
-  adn varchar(20)  NOT NULL,
-  dkodu varchar(20) NULL,
-  ders_adi varchar(50) NULL,
-  teo varchar(10)  NULL,
-  uyg varchar(10)  NULL,
-  sube varchar(20)  NULL,
-  SINIF varchar(2)  NULL,
-  DersGrubu varchar(10) NULL,
-  egitmen varchar(30)  NULL,
-  PRIMARY KEY (birim,adn)
-);
-
-CREATE TABLE dprog (
-  birim varchar(20) DEFAULT NULL,
-  adn varchar(20) DEFAULT NULL,
-  dilim varchar(20) DEFAULT NULL,
-  sure varchar(20) DEFAULT NULL,
-  teo_uyg varchar(20) DEFAULT NULL,
-  foreign key(birim, adn) references dersler(birim,adn)
-) ;
 
 
 
+CREATE TABLE `bolumler` (
+  `birim` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `birim_adi` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`birim`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-USE schedularv3;
-DROP procedure IF EXISTS getbirimler;
-DELIMITER 
-USE schedularv3 
-CREATE PROCEDURE getbirimler ()
+
+CREATE TABLE `dersler` (
+  `birim` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `adn` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `dkodu` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ders_adi` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `teo` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `uyg` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sube` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `SINIF` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `DersGrubu` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `egitmen` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`birim`,`adn`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `dprog` (
+  `birim` varchar(20) DEFAULT NULL,
+  `adn` varchar(20) DEFAULT NULL,
+  `dilim` varchar(20) DEFAULT NULL,
+  `sure` varchar(20) DEFAULT NULL,
+  `teo_uyg` varchar(20) DEFAULT NULL,
+  KEY `birim` (`birim`,`adn`),
+  KEY `fk_adn` (`adn`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='foreign key(birim, adn) references dersler(birim,adn)';
+
+
+
+
+USE `schedularv3`;
+DROP procedure IF EXISTS `getbirimler`;
+DELIMITER $$
+USE `schedularv3`$$
+CREATE PROCEDURE `getbirimler` ()
 BEGIN
 SELECT birim_adi,birim FROM bolumler;
 END$$
@@ -80,33 +85,3 @@ SELECT adn ,dilim,sure,teo_uyg FROM dprog WHERE adn IN ('+array+');
 END$$
 DELIMITER ;
 -- bu procedure çalışmamaktadır array pass olmuyor
-
-
-
-
-
-
-USE schedularv3;  
-GO  
-CREATE PROCEDURE getbirimler   
-    @birim_val nvarchar(20),   
-    @sinif_val nvarchar(20)   
-AS   
-
-    SET NOCOUNT ON;  
-  SELECT adn,birim,egitmen,dkodu,ders_adi,teo,uyg,sube,DersGrubu,SINIF FROM dersler where birim=@birim_val AND SINIF=@sinif_val ORDER BY ders_adi desc;
-
-GO  
-
-
-
-
-USE schedularv3;  
-GO  
-CREATE PROCEDURE getbirimler   
-AS   
-
-    SET NOCOUNT ON;  
-SELECT birim_adi,birim FROM bolumler;
-
-GO  
